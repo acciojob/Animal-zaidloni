c7da4608cf75adbdb69f6ee9364e3e58709b6943
Zaid Loni <96578621+zaidloni@users.noreply.github.com>
2023-02-27 12:06:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+window.addEventListener("load", function() {
+    const images = document.querySelectorAll(".photo-container img");
+    let loadedImages = 0;
+    
+    images.forEach(function(image) {
+      image.addEventListener("load", function() {
+        loadedImages++;
+        
+        if (loadedImages === images.length) {
+          document.querySelector(".photo-container").style.visibility = "visible";
+        }
+      });
+    });
+  });
